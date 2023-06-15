a841922a40957d448e5aad4360478a35f5b9ee89
Ryan ingyu hwang <dlsrb07@gmail.com>
2023-06-15 15:22:10 +0900

    6.8 done

--- lib/features/main_navigation/main_navigation.dart
+++ lib/features/main_navigation/main_navigation.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 import 'package:tiktok/features/main_navigation/stf_screen.dart';
-import 'package:tiktok/features/onboarding/widgets/nav_tab.dart';
+import 'package:tiktok/features/main_navigation/widgets/nav_tab.dart';
+import 'package:tiktok/features/main_navigation/widgets/post_video_button.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -14,53 +16,18 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = [
-    StfScreen(
-      key: GlobalKey(),
-    ),
-    StfScreen(
-      key: GlobalKey(),
-    ),
-    StfScreen(
-      key: GlobalKey(),
-    ),
-    StfScreen(
-      key: GlobalKey(),
-    ),
-    StfScreen(
-      key: GlobalKey(),
-    ),
-    // const Center(
-    //   child: Text(
-    //     'Home',
-    //     style: TextStyle(fontSize: 49),
-    //   ),
-    // ),
-    // const Center(
-    //   child: Text(
-    //     'Discover',
-    //     style: TextStyle(fontSize: 49),
-    //   ),
-    // ),
-    // Container(),
-    // const Center(
-    //   child: Text(
-    //     'Inbox',
-    //     style: TextStyle(fontSize: 49),
-    //   ),
-    // ),
-    // const Center(
-    //   child: Text(
-    //     'Profile',
-    //     style: TextStyle(fontSize: 49),
-    //   ),
-    // ),
-  ];
-
   void _onTap(int index) {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => Scaffold(
+        appBar: AppBar(title: const Text('Record Video')),
+      ),
+    ));
   }
 
   @override
@@ -87,43 +54,49 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
         ],
       ),
       bottomNavigationBar: BottomAppBar(
-          color: Colors.black,
-          child: Padding(
-            padding: const EdgeInsets.all(Sizes.size12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                NavTab(
-                  text: "Home",
-                  isSelected: _selectedIndex == 0,
-                  icon: FontAwesomeIcons.house,
-                  selectedIcon: FontAwesomeIcons.house,
-                  onTap: () => _onTap(0),
-                ),
-                NavTab(
-                  text: "Discover",
-                  isSelected: _selectedIndex == 1,
-                  icon: FontAwesomeIcons.compass,
-                  selectedIcon: FontAwesomeIcons.solidCompass,
-                  onTap: () => _onTap(1),
-                ),
-                NavTab(
-                  text: "Inbox",
-                  isSelected: _selectedIndex == 3,
-                  icon: FontAwesomeIcons.message,
-                  selectedIcon: FontAwesomeIcons.solidMessage,
-                  onTap: () => _onTap(3),
-                ),
-                NavTab(
-                  text: "Profile",
-                  isSelected: _selectedIndex == 4,
-                  icon: FontAwesomeIcons.user,
-                  selectedIcon: FontAwesomeIcons.solidUser,
-                  onTap: () => _onTap(4),
-                ),
-              ],
-            ),
-          )),
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              NavTab(
+                text: "Home",
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
+                onTap: () => _onTap(1),
+              ),
+              Gaps.h24,
+              GestureDetector(
+                  onTap: _onPostVideoButtonTap, child: const PostVideoButton()),
+              Gaps.h24,
+              NavTab(
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
