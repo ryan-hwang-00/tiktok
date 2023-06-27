57bdec11e53b38bfac8ae459f7a5cc3c61693525
Ryan ingyu hwang <dlsrb07@gmail.com>
2023-06-27 13:54:09 +0900

    8.1 done

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -24,25 +25,91 @@ class _VideoCommentsState extends State<VideoComments> {
         ),
       ),
       child: Scaffold(
-        backgroundColor: Colors.grey.shade50,
-        appBar: AppBar(
           backgroundColor: Colors.grey.shade50,
-          title: const Text("12341"),
-          automaticallyImplyLeading: false,
-          actions: [
-            IconButton(
-              onPressed: _onClosePressed,
-              icon: const FaIcon(FontAwesomeIcons.xmark),
-            ),
-          ],
-        ),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            child: const Text("im a comment"),
+          appBar: AppBar(
+            backgroundColor: Colors.grey.shade50,
+            title: const Text("12341"),
+            automaticallyImplyLeading: false,
+            actions: [
+              IconButton(
+                onPressed: _onClosePressed,
+                icon: const FaIcon(FontAwesomeIcons.xmark),
+              ),
+            ],
           ),
-        ),
-      ),
+          body: ListView.separated(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size10,
+              horizontal: Sizes.size16,
+            ),
+            separatorBuilder: (context, index) => Gaps.v20,
+            itemCount: 10,
+            itemBuilder: (context, index) => Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  // foregroundImage: NetworkImage(
+                  //     "https://avatars.githubusercontent.com/u/68258601?v=4"),
+                  child: Text("nico"),
+                ),
+                Gaps.h10,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'ingyu hwang',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: Sizes.size14,
+                          color: Colors.grey.shade500,
+                        ),
+                      ),
+                      Gaps.v3,
+                      const Text(
+                          'some kind of text some kind of text some kind of text some kind of text some kind of text some kind of text some kind of text some kind of text some kind of text some kind of text some kind of text some kind of text'),
+                    ],
+                  ),
+                ),
+                Gaps.h10,
+                Column(
+                  children: [
+                    FaIcon(
+                      FontAwesomeIcons.heart,
+                      size: Sizes.size20,
+                      color: Colors.grey.shade500,
+                    ),
+                    Gaps.v2,
+                    Text(
+                      '2.2K',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          bottomNavigationBar: BottomAppBar(
+            color: Colors.white,
+            child: Row(
+              children: const [
+                CircleAvatar(
+                  radius: 18,
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  // foregroundImage: NetworkImage(
+                  //     "https://avatars.githubusercontent.com/u/68258601?v=4"),
+                  child: Text("nico"),
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
